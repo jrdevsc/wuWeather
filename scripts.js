63d60a4a20c29870443ad7a47aee7cceb30a681c
julian rudolph <julian.rudolphsc@gmail.com>
2016-12-29 09:58:45 -0500

    finished ajax and appending dom

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+// hosted at https://jrdevsc.github.io/wuWeather/
+
+
+
 var key = "5f0f96c76a3e03cf";
 var url = "https://api.wunderground.com/api/" +key;
 
@@ -9,7 +13,19 @@ $.ajax({
       url: "https://api.wunderground.com/api/5f0f96c76a3e03cf/conditions/q/"+location+".json",
       dataType: "jsonp",
       success: function(lweather){
-        console.log(lweather);
+        var simpleCall = lweather.current_observation;
+
+        //append city
+        $('#city').append("<h1>"+simpleCall.display_location.city+"</h1>");
+
+        //append temp
+        $('#temp').append("<h2>Temp:</h2><h3>"+simpleCall.temp_f+"</h3>");
+
+        //append current weather conditions
+        $('#cWeather').append("<h2>Currently:</h2><h3>"+simpleCall.weather+"</h3>");
+
+        //append wind
+        $('#wind').append("<h2>Wind Conditions:</h2><p>"+simpleCall.wind_string+"<br> Direction: "+simpleCall.wind_dir+"<br> Wind Chill: "+simpleCall.windchill_f+"</p>");
       }
     });
   }
