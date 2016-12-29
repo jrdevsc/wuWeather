0d62460dece997da45569ccafe579c3a7a5d9c70
julian rudolph <julian.rudolphsc@gmail.com>
2016-12-29 09:19:40 -0500

    initial commit, works locally

---
+++ scripts.js
@@ -0,0 +1,16 @@
+var key = "5f0f96c76a3e03cf";
+var url = "http://api.wunderground.com/api/" +key;
+
+$.ajax({
+  url: url +"/geolookup/q/autoip.json",
+  success: function(data){
+    var location = data.location.requesturl;
+    $.getJSON({
+      url: "https://api.wunderground.com/api/5f0f96c76a3e03cf/conditions/q/"+location+".json",
+      dataType: "jsonp",
+      success: function(lweather){
+        console.log(lweather);
+      }
+    });
+  }
+});
