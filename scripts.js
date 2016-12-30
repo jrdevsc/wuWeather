e41bcb0358687b00aca8b4839f5ed7a828d2fb28
julian rudolph <julian.rudolphsc@gmail.com>
2016-12-30 09:52:23 -0500

    added c/f toggle button

--- scripts.js
+++ scripts.js
@@ -18,16 +18,32 @@ $.ajax({
         var icon = simpleCall.icon_url;
 
         //append city
-        $('#city').append("<h1>"+simpleCall.display_location.city+"</h1>");
+        $('#city').replaceWith("<h1>"+simpleCall.display_location.city+"</h1>");
 
         //append temp
         $('#temp').append("<h2>Temp:</h2><h3>"+simpleCall.temp_f+"</h3>");
 
         //append current weather conditions
-        $('#cWeather').append("<h2>Currently:</h2><h3>"+simpleCall.weather+"</h3> <img src="+icon+" />");
+        $('#cWeather').replaceWith("<h2>Currently:</h2><h3>"+simpleCall.weather+"</h3> <img src="+icon+" />");
 
         //append wind
-        $('#wind').append("<h2>Wind Conditions:</h2><p>"+simpleCall.wind_string+"<br> Direction: "+simpleCall.wind_dir+"<br> Wind Chill: "+simpleCall.windchill_f+"</p>");
+        $('#wind').replaceWith("<h2>Wind Conditions:</h2><p>"+simpleCall.wind_string+"<br> Direction: "+simpleCall.wind_dir+"<br> Wind Chill: "+simpleCall.windchill_f+"</p>");
+
+        //convert temp on button click
+        $('#chk').click(function(){
+          var fTemp = parseInt(simpleCall.temp_f);
+          var cTemp = parseInt(simpleCall.temp_c);
+
+          if ($(this).text() == "Get Temp in F") {
+                  $('#temp').html("<h2>Temp:</h2><h3>"+fTemp+" F</h3>");
+                  $(this).text("Get Temp in C");
+                }else if($(this).text() == "Get Temp in C"){
+                  $('#temp').html("<h2>Temp:</h2><h3>"+cTemp+" C</h3>");
+                  $(this).text("Get Temp in F");
+                }else{
+                  $(this).text("Should never fire...");
+                }
+        });
       }
     });
   }
